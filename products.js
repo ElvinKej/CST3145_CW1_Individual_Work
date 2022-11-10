5ac20b21449698f8b3ab76412ecb36cb81038f49
Elvin <elvinkejiou27@gmail.com>
2022-11-10 17:55:20 +0000

    Added products.js file

---
+++ products.js
@@ -0,0 +1,92 @@
+let products = [
+    {
+        "id": 101,
+        "subject": "Math",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 102,
+        "subject": "English",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 103,
+        "subject": "Science",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 104,
+        "subject": "History",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 105,
+        "subject": "Geography",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 106,
+        "subject": "Music",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 107,
+        "subject": "Spanish",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 108,
+        "subject": "French",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 109,
+        "subject": "Art",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    },
+    {
+        "id": 110,
+        "subject": "Economics",
+        "location": "London",
+        "price": "£100",
+        "space": 5,
+        "icon": "image",
+        "Image": "images/"
+    }
+]
