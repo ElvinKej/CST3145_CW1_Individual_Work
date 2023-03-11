d892253b7d3d17f54e256209e2e3e3c248a66a02
ElvinKej <elvinkejiou27@gmail.com>
2023-03-11 20:39:15 +0000

    service-worker updated

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName = "webstore-v1";
 var cacheFiles = [
     "index.html",
-    //"products.js",
+    "products.js",
     "Pictures/Art.png",
     "Pictures/Economics.png",
     "Pictures/English.png",
@@ -20,4 +20,34 @@ self.addEventListener("install", function(e) {
         console.log("[Service Worker] Caching files");
         return cache.addAll(cacheFiles);
     }));
+});
+
+//Static Caching
+self.addEventListener('fetch', function (e) {
+    e.respondWith (
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource:' + e.request.url);
+            return r;
+        })
+    );
+});
+
+//Dynamic Caching
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else { 
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " + e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    );
 });
